2e2b6c82d95f41deed1ee42948bf96804c08bc63
Aaron Heidgerken-Greene <aheidgerken@smm.org>
2015-12-06 15:55:08 -0600

    adapting for hardware changes.

--- background.js
+++ background.js
@@ -8,8 +8,8 @@ chrome.app.runtime.onLaunched.addListener(function(launchData) {
   chrome.app.window.create(
     'index.html',
     {
-      id: 'mainWindow',
-      bounds: {width: 800, height: 600},
+      //id: 'mainWindow',
+      //bounds: {width: 800, height: 600},
       state: 'fullscreen',
     },
     function(win) {win.contentWindow.isApp = true;}
