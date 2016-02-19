5369c737838ee6ce634d9e8b10fcabe772a890c3
Aaron Heidgerken-Greene <heidgera@gmail.com>
2016-02-19 15:24:03 -0600

    new bat file

--- startup.bat
+++ startup.bat
@@ -1,1 +1,1 @@
-start chrome --user-data-dir="C:\Users\escape\chromeData" --disable-info-bars --load-and-launch-app="C:\Users\escape\Desktop\app"
+start chrome --disable-info-bars --app-id=bihnaimahfngaliflknnkgokppgkejfn
